9f3a750f563dd7d8e30735a0d7cae92e1098c094
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2020-03-01 08:23:38 -0600

    Update url to reflect OcSupportPkg merge

--- MakeInstall.py
+++ MakeInstall.py
@@ -28,7 +28,7 @@ class WinUSB:
         self.oc_boot = "boot"
         self.oc_boot0 = "boot0af"
         self.oc_boot1 = "boot1f32"
-        self.oc_boot_url = "https://github.com/acidanthera/OcSupportPkg/raw/master/Utilities/BootInstall/"
+        self.oc_boot_url = "https://github.com/acidanthera/OpenCorePkg/raw/master/Utilities/BootInstall/"
         self.diskpart = os.path.join(os.environ['SYSTEMDRIVE'] + "\\", "Windows", "System32", "diskpart.exe")
         # From Tim Sutton's brigadier:  https://github.com/timsutton/brigadier/blob/master/brigadier
         self.z_path = None
