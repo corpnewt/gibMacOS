25c411355f7a3abc54d3cdd1e766cc43e0103064
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2025-06-01 10:05:46 -0500

    Bring to feature parity

--- Scripts/plist.py
+++ Scripts/plist.py
@@ -2,13 +2,15 @@
 # Imports #
 ###     ###
 
-import datetime, os, plistlib, struct, sys, itertools
+import datetime, os, plistlib, struct, sys, itertools, binascii
 from io import BytesIO
 
 if sys.version_info < (3,0):
     # Force use of StringIO instead of cStringIO as the latter
     # has issues with Unicode strings
     from StringIO import StringIO
+else:
+    from io import StringIO
 
 try:
     basestring  # Python 2
@@ -67,8 +69,16 @@ def writePlist(value, pathOrFile):
 ###                ###
 
 def load(fp, fmt=None, use_builtin_types=None, dict_type=dict):
-    if _check_py3():
-        use_builtin_types = True if use_builtin_types == None else use_builtin_types
+    if _is_binary(fp):
+        use_builtin_types = False if use_builtin_types is None else use_builtin_types
+        try:
+            p = _BinaryPlistParser(use_builtin_types=use_builtin_types, dict_type=dict_type)
+        except:
+            # Python 3.9 removed use_builtin_types
+            p = _BinaryPlistParser(dict_type=dict_type)
+        return p.parse(fp)
+    elif _check_py3():
+        use_builtin_types = True if use_builtin_types is None else use_builtin_types
         # We need to monkey patch this to allow for hex integers - code taken/modified from 
         # https://github.com/python/cpython/blob/3.8/Lib/plistlib.py
         if fmt is None:
@@ -92,7 +102,7 @@ def load(fp, fmt=None, use_builtin_types=None, dict_type=dict):
             def end_integer():
                 d = p.get_data()
                 value = int(d,16) if d.lower().startswith("0x") else int(d)
-                if -1 << 63 <= value < 1 << 63:
+                if -1 << 63 <= value < 1 << 64:
                     p.add_object(value)
                 else:
                     raise OverflowError("Integer overflow at line {}".format(p.parser.CurrentLineNumber))
@@ -104,7 +114,7 @@ def load(fp, fmt=None, use_builtin_types=None, dict_type=dict):
             p.end_integer = end_integer
             p.end_data = end_data
         return p.parse(fp)
-    elif not _is_binary(fp):
+    else:
         # Is not binary - assume a string - and try to load
         # We avoid using readPlistFromString() as that uses
         # cStringIO and fails when Unicode strings are detected
@@ -126,7 +136,7 @@ def load(fp, fmt=None, use_builtin_types=None, dict_type=dict):
         def end_integer():
             d = p.getData()
             value = int(d,16) if d.lower().startswith("0x") else int(d)
-            if -1 << 63 <= value < 1 << 63:
+            if -1 << 63 <= value < 1 << 64:
                 p.addObject(value)
             else:
                 raise OverflowError("Integer overflow at line {}".format(parser.CurrentLineNumber))
@@ -153,14 +163,6 @@ def load(fp, fmt=None, use_builtin_types=None, dict_type=dict):
         # Parse it
         parser.ParseFile(fp)
         return p.root
-    else:
-        use_builtin_types = False if use_builtin_types == None else use_builtin_types
-        try:
-            p = _BinaryPlistParser(use_builtin_types=use_builtin_types, dict_type=dict_type)
-        except:
-            # Python 3.9 removed use_builtin_types
-            p = _BinaryPlistParser(dict_type=dict_type)
-        return p.parse(fp)
 
 def loads(value, fmt=None, use_builtin_types=None, dict_type=dict):
     if _check_py3() and isinstance(value, basestring):
@@ -171,11 +173,16 @@ def loads(value, fmt=None, use_builtin_types=None, dict_type=dict):
     except:
         # Python 3.9 removed use_builtin_types
         return load(BytesIO(value),fmt=fmt,dict_type=dict_type)
+
 def dump(value, fp, fmt=FMT_XML, sort_keys=True, skipkeys=False):
-    if _check_py3():
-        plistlib.dump(value, fp, fmt=fmt, sort_keys=sort_keys, skipkeys=skipkeys)
-    else:
-        if fmt == FMT_XML:
+    if fmt == FMT_BINARY:
+        # Assume binary at this point
+        writer = _BinaryPlistWriter(fp, sort_keys=sort_keys, skipkeys=skipkeys)
+        writer.write(value)
+    elif fmt == FMT_XML:
+        if _check_py3():
+            plistlib.dump(value, fp, fmt=fmt, sort_keys=sort_keys, skipkeys=skipkeys)
+        else:
             # We need to monkey patch a bunch here too in order to avoid auto-sorting
             # of keys
             writer = plistlib.PlistWriter(fp)
@@ -197,30 +204,63 @@ def dump(value, fp, fmt=FMT_XML, sort_keys=True, skipkeys=False):
             writer.writeln("<plist version=\"1.0\">")
             writer.writeValue(value)
             writer.writeln("</plist>")
-        elif fmt == FMT_BINARY:
-            # Assume binary at this point
-            writer = _BinaryPlistWriter(fp, sort_keys=sort_keys, skipkeys=skipkeys)
-            writer.write(value)
-        else:
-            # Not a proper format
-            raise ValueError("Unsupported format: {}".format(fmt))
+    else:
+        # Not a proper format
+        raise ValueError("Unsupported format: {}".format(fmt))
     
 def dumps(value, fmt=FMT_XML, skipkeys=False, sort_keys=True):
+    # We avoid using writePlistToString() as that uses
+    # cStringIO and fails when Unicode strings are detected
+    f = BytesIO() if _check_py3() else StringIO()
+    dump(value, f, fmt=fmt, skipkeys=skipkeys, sort_keys=sort_keys)
+    value = f.getvalue()
     if _check_py3():
-        return plistlib.dumps(value, fmt=fmt, skipkeys=skipkeys, sort_keys=sort_keys).decode("utf-8")
-    else:
-        # We avoid using writePlistToString() as that uses
-        # cStringIO and fails when Unicode strings are detected
-        f = StringIO()
-        dump(value, f, fmt=fmt, skipkeys=skipkeys, sort_keys=sort_keys)
-        return f.getvalue()
+        value = value.decode("utf-8")
+    return value
 
 ###                        ###
 # Binary Plist Stuff For Py2 #
 ###                        ###
 
-# From the python 3 plistlib.py source:  https://github.com/python/cpython/blob/3.7/Lib/plistlib.py
-# Tweaked to function on Python 2
+# From the python 3 plistlib.py source:  https://github.com/python/cpython/blob/3.11/Lib/plistlib.py
+# Tweaked to function on both Python 2 and 3
+
+class UID:
+    def __init__(self, data):
+        if not isinstance(data, int):
+            raise TypeError("data must be an int")
+        # It seems Apple only uses 32-bit unsigned ints for UIDs. Although the comment in
+        # CoreFoundation's CFBinaryPList.c detailing the binary plist format theoretically
+        # allows for 64-bit UIDs, most functions in the same file use 32-bit unsigned ints,
+        # with the sole function hinting at 64-bits appearing to be a leftover from copying
+        # and pasting integer handling code internally, and this code has not changed since
+        # it was added. (In addition, code in CFPropertyList.c to handle CF$UID also uses a
+        # 32-bit unsigned int.)
+        #
+        # if data >= 1 << 64:
+        #    raise ValueError("UIDs cannot be >= 2**64")
+        if data >= 1 << 32:
+            raise ValueError("UIDs cannot be >= 2**32 (4294967296)")
+        if data < 0:
+            raise ValueError("UIDs must be positive")
+        self.data = data
+
+    def __index__(self):
+        return self.data
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, repr(self.data))
+
+    def __reduce__(self):
+        return self.__class__, (self.data,)
+
+    def __eq__(self, other):
+        if not isinstance(other, UID):
+            return NotImplemented
+        return self.data == other.data
+
+    def __hash__(self):
+        return hash(self.data)
 
 class InvalidFileException (ValueError):
     def __init__(self, message="Invalid file"):
@@ -269,7 +309,10 @@ class _BinaryPlistParser:
     def _get_size(self, tokenL):
         """ return the size of the next object."""
         if tokenL == 0xF:
-            m = ord(self._fp.read(1)[0]) & 0x3
+            m = self._fp.read(1)[0]
+            if not _check_py3():
+                m = ord(m)
+            m = m & 0x3
             s = 1 << m
             f = '>' + _BINARY_FORMAT[s]
             return struct.unpack(f, self._fp.read(s))[0]
@@ -283,8 +326,10 @@ class _BinaryPlistParser:
         else:
             if not size or len(data) != size * n:
                 raise InvalidFileException()
-            return tuple(int.from_bytes(data[i: i + size], 'big')
+            return tuple(int(binascii.hexlify(data[i: i + size]),16)
                          for i in range(0, size * n, size))
+            '''return tuple(int.from_bytes(data[i: i + size], 'big')
+                         for i in range(0, size * n, size))'''
 
     def _read_refs(self, n):
         return self._read_ints(n, self._ref_size)
@@ -300,30 +345,30 @@ class _BinaryPlistParser:
 
         offset = self._object_offsets[ref]
         self._fp.seek(offset)
-        token = ord(self._fp.read(1)[0])
+        token = self._fp.read(1)[0]
+        if not _check_py3():
+            token = ord(token)
         tokenH, tokenL = token & 0xF0, token & 0x0F
 
-        if token == 0: # \x00 or 0x00
+        if token == 0x00: # \x00 or 0x00
             result = None
 
-        elif token == 8: # \x08 or 0x08
+        elif token == 0x08: # \x08 or 0x08
             result = False
 
-        elif token == 9: # \x09 or 0x09
+        elif token == 0x09: # \x09 or 0x09
             result = True
 
         # The referenced source code also mentions URL (0x0c, 0x0d) and
         # UUID (0x0e), but neither can be generated using the Cocoa libraries.
 
-        elif token == 15: # \x0f or 0x0f
+        elif token == 0x0f: # \x0f or 0x0f
             result = b''
 
         elif tokenH == 0x10:  # int
-            result = 0
-            for k in range((2 << tokenL) - 1):
-                result = (result << 8) + ord(self._fp.read(1))
-            # result = int.from_bytes(self._fp.read(1 << tokenL),
-            #                        'big', signed=tokenL >= 3)
+            result = int(binascii.hexlify(self._fp.read(1 << tokenL)),16)
+            if tokenL >= 3: # Signed - adjust
+                result = result-((result & 0x8000000000000000) << 1)
 
         elif token == 0x22: # real
             result = struct.unpack('>f', self._fp.read(4))[0]
@@ -340,7 +385,7 @@ class _BinaryPlistParser:
 
         elif tokenH == 0x40:  # data
             s = self._get_size(tokenL)
-            if self._use_builtin_types:
+            if self._use_builtin_types or not hasattr(plistlib, "Data"):
                 result = self._fp.read(s)
             else:
                 result = plistlib.Data(self._fp.read(s))
@@ -354,8 +399,9 @@ class _BinaryPlistParser:
             s = self._get_size(tokenL)
             result = self._fp.read(s * 2).decode('utf-16be')
 
-        # tokenH == 0x80 is documented as 'UID' and appears to be used for
-        # keyed-archiving, not in plists.
+        elif tokenH == 0x80:  # UID
+            # used by Key-Archiver plist files
+            result = UID(int(binascii.hexlify(self._fp.read(1 + tokenL)),16))
 
         elif tokenH == 0xA0:  # array
             s = self._get_size(tokenL)
@@ -378,7 +424,7 @@ class _BinaryPlistParser:
             self._objects[ref] = result
             for k, o in zip(key_refs, obj_refs):
                 key = self._read_object(k)
-                if isinstance(key, plistlib.Data):
+                if hasattr(plistlib, "Data") and isinstance(key, plistlib.Data):
                     key = key.data
                 result[key] = self._read_object(o)
 
@@ -395,7 +441,7 @@ def _count_to_size(count):
     elif count < 1 << 16:
         return 2
 
-    elif count << 1 << 32:
+    elif count < 1 << 32:
         return 4
 
     else:
@@ -462,7 +508,7 @@ class _BinaryPlistWriter (object):
             if (type(value), value) in self._objtable:
                 return
 
-        elif isinstance(value, plistlib.Data):
+        elif hasattr(plistlib, "Data") and isinstance(value, plistlib.Data):
             if (type(value.data), value.data) in self._objtable:
                 return
 
@@ -474,7 +520,7 @@ class _BinaryPlistWriter (object):
         self._objlist.append(value)
         if isinstance(value, _scalars):
             self._objtable[(type(value), value)] = refnum
-        elif isinstance(value, plistlib.Data):
+        elif hasattr(plistlib, "Data") and isinstance(value, plistlib.Data):
             self._objtable[(type(value.data), value.data)] = refnum
         else:
             self._objidtable[id(value)] = refnum
@@ -505,7 +551,7 @@ class _BinaryPlistWriter (object):
     def _getrefnum(self, value):
         if isinstance(value, _scalars):
             return self._objtable[(type(value), value)]
-        elif isinstance(value, plistlib.Data):
+        elif hasattr(plistlib, "Data") and isinstance(value, plistlib.Data):
             return self._objtable[(type(value.data), value.data)]
         else:
             return self._objidtable[id(value)]
@@ -564,9 +610,11 @@ class _BinaryPlistWriter (object):
             f = (value - datetime.datetime(2001, 1, 1)).total_seconds()
             self._fp.write(struct.pack('>Bd', 0x33, f))
 
-        elif isinstance(value, plistlib.Data):
-            self._write_size(0x40, len(value.data))
-            self._fp.write(value.data)
+        elif (_check_py3() and isinstance(value, (bytes, bytearray))) or (hasattr(plistlib, "Data") and isinstance(value, plistlib.Data)):
+            if not isinstance(value, (bytes, bytearray)):
+                value = value.data # Unpack it
+            self._write_size(0x40, len(value))
+            self._fp.write(value)
 
         elif isinstance(value, basestring):
             try:
@@ -576,10 +624,20 @@ class _BinaryPlistWriter (object):
                 t = value.encode('utf-16be')
                 self._write_size(0x60, len(t) // 2)
             self._fp.write(t)
-        
-        elif isinstance(value, (bytes, bytearray)):
-            self._write_size(0x40, len(value))
-            self._fp.write(value)
+
+        elif isinstance(value, UID) or (hasattr(plistlib,"UID") and isinstance(value, plistlib.UID)):
+            if value.data < 0:
+                raise ValueError("UIDs must be positive")
+            elif value.data < 1 << 8:
+                self._fp.write(struct.pack('>BB', 0x80, value))
+            elif value.data < 1 << 16:
+                self._fp.write(struct.pack('>BH', 0x81, value))
+            elif value.data < 1 << 32:
+                self._fp.write(struct.pack('>BL', 0x83, value))
+            # elif value.data < 1 << 64:
+            #    self._fp.write(struct.pack('>BQ', 0x87, value))
+            else:
+                raise OverflowError(value)
 
         elif isinstance(value, (list, tuple)):
             refs = [self._getrefnum(o) for o in value]
